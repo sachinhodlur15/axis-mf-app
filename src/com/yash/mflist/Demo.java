37221ce410fd204c63d78caa33df45ea20e86082
sachinhodlur15 <group.amazon.prime1@gmail.com>
2024-03-26 19:15:41 +0530

    ADAT-1000: Addressed comment

--- src/com/yash/mflist/Demo.java
+++ src/com/yash/mflist/Demo.java
@@ -3,7 +3,7 @@ package com.yash.mflist;
 public class Demo {
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to MF!!!");
+		System.out.println("Welcome to Mutual Fund!!!");
 	}
 
 }
